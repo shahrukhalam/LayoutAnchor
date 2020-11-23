89670f54b9f0a9438af7f0a641cbde41443e9dc0
Shahrukh Alam <shahrukh.alam@paytmmoney.com>
2020-11-23 22:27:45 +0530

    Initial Commit

---
+++ LayoutAnchor/LayoutAnchor.h
@@ -0,0 +1,18 @@
+//
+//  LayoutAnchor.h
+//  LayoutAnchor
+//
+//  Created by Shahrukh Alam on 23/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LayoutAnchor.
+FOUNDATION_EXPORT double LayoutAnchorVersionNumber;
+
+//! Project version string for LayoutAnchor.
+FOUNDATION_EXPORT const unsigned char LayoutAnchorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LayoutAnchor/PublicHeader.h>
+
+
